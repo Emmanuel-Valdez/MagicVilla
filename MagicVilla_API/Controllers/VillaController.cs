0608f249e2828ede438f0eb58b64158f8cd738e3
Emmanuel Valdez <evaldez.mza@gmail.com>
2023-07-07 12:44:38 -0300

    Creados primeros endpoint

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class VillaController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<VillaDto> GetVillas()
+		{
+			return VillaStore.villasList;
+		}
+		[HttpGet("id")]
+		public VillaDto GetVilla(int id)
+		{
+			return VillaStore.villasList.FirstOrDefault(v => v.Id == id);
+		}
+	}
+}
